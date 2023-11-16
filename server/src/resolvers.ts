5a635d563c7fb10edeb2f8484f5a8d016e88febe
Женя Короленко <zenakorolenko@MacBook-Pro-iMag-2.local>
2023-11-16 21:41:29 +0200

    mutation for likes was added

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,20 +1,22 @@
-import Query from './resolvers/Query'
-import User from './resolvers/User'
-import Tweet from './resolvers/Tweet'
-import Db, { DbTweet, DbUser } from "./db"
-import { Resolvers } from "./resolvers-types.generated"
+import Query from './resolvers/Query';
+import User from './resolvers/User';
+import Tweet from './resolvers/Tweet';
+import Mutation from './resolvers/Mutation';
+import Db, { DbTweet, DbUser } from './db';
+import { Resolvers } from './resolvers-types.generated';
 
 export interface TwitterResolverContext {
-  db: Db,
-  dbTweetCache: Record<string, DbTweet>,
-  dbTweetToFavoriteCountMap: Record<string, number>,
-  dbUserCache: Record<string, DbUser>,
+  db: Db;
+  dbTweetCache: Record<string, DbTweet>;
+  dbTweetToFavoriteCountMap: Record<string, number>;
+  dbUserCache: Record<string, DbUser>;
 }
 
 const resolvers: Resolvers<TwitterResolverContext> = {
   Query,
   User,
+  Mutation,
   Tweet,
-}
+};
 
 export default resolvers;
--- server/src/resolvers/User.ts
+++ server/src/resolvers/User.ts
@@ -1,13 +1,24 @@
-import { TwitterResolverContext } from "../resolvers"
-import { UserResolvers } from "../resolvers-types.generated"
-const userTwitterResolver: UserResolvers<TwitterResolverContext> =
-  {
-    stats(user, _, { db }) {
+import { TwitterResolverContext } from '../resolvers';
+import { UserResolvers } from '../resolvers-types.generated';
+import { favoriteTransform, tweetTransform } from '../transforms';
+
+const userTwitterResolver: UserResolvers<TwitterResolverContext> = {
+  stats(user, _, { db }) {
+    return {
+      followingCount: 123,
+      followerCount: 456789,
+      tweetCount: db.getUserTweets(user.id).length,
+    };
+  },
+  favorites(user, _, { db }) {
+    const faves = db.getUserFavorites(user.id);
+    return faves.map((f) => {
       return {
-        followingCount: 123,
-        followerCount: 456789,
-        tweetCount: db.getUserTweets(user.id).length,
-      }
-    },
-  }
-export default userTwitterResolver
+        ...favoriteTransform(f),
+        user,
+        tweet: tweetTransform(db.getTweetById(f.tweetId)),
+      };
+    });
+  },
+};
+export default userTwitterResolver;
